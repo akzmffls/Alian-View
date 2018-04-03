8ec60f76051d969e390459d03916305f56f6a2b4
akzmffls <d7703_14@d7703-14ui-imac.local>
2018-04-03 15:41:49 +0900

    Alien view order

--- Alian View/ViewController.swift
+++ Alian View/ViewController.swift
@@ -11,14 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     
     var counter = 1
+    var mom = true
 
     @IBOutlet var alienImgeView: UIImageView!
+    @IBOutlet weak var lbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // 1st 이미지가 출력
         alienImgeView.image = UIImage(named: "frame1.png")
+        lbl.text = "1"
         
     }
 
@@ -27,12 +30,22 @@ class ViewController: UIViewController {
 
     @IBAction func updateImage(_ sender: Any) {
         
-        if counter == 5 {
-            counter = 1
-        } else {
-            counter = counter + 1
+        if counter == 1 {
+            mom = true
+            
+        } else if counter == 5{
+            mom = false
+            
+            
             }
+        if mom == true {
+            counter = counter + 1
+        } else if mom == false {
+            counter = counter - 1
+        }
+        
         alienImgeView.image = UIImage(named: "frame\(counter).png")
+        lbl.text = String(counter)
         }
     }
 
